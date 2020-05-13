fbc1af84e2087ed928dc5b5c74c5092c00694aba
yunliang <yunliang@ding.com>
2020-05-13 14:17:53 +0800

    worker

--- src/store/worker/index.ts
+++ src/store/worker/index.ts
@@ -4,22 +4,40 @@ import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
 import { fromCode } from '../../axios/util'
 class WorkerStore {
   worker: Worker
-  loop = (data) => {
-    data.map(file => {
-      if (file.type === 'file') {
-        ['.ts', '.tsx'].indexOf(file.extension) > -1 && this.worker.postMessage(file.path)
-      } else {
-        this.loop(file.children)
+  init = () => {
+    this.worker = this.createWorker(() => {
+      const axios = new XMLHttpRequest();
+      const createModel = (data) => {
+        data.map(file => {
+          if (file.type === 'file') {
+            if (['.ts', '.tsx'].indexOf(file.extension) > -1) {
+              axios.open("GET", `${location.origin}/api/file/getfile?path=${file.path}`, false);
+              axios.send();
+              const { data, isError } = JSON.parse(axios.responseText)
+              if (!isError) {
+                self.postMessage(JSON.stringify({ content: data, path: file.path }), null)
+              }
+            }
+          } else {
+            createModel(file.children)
+          }
+        })
+      }
+      onmessage = (event) => {
+        createModel(JSON.parse(event.data))
       }
     })
-  }
-  init = () => {
-    this.worker = new Worker('./static/js/work.js');
     this.worker.onmessage = (event: any) => {
       const { content, path } = JSON.parse(event.data)
       monaco.editor.createModel(fromCode(content), 'typescript', monacoService.getUri(path))
     }
-    this.loop(fileSystem.files.children)
+    this.worker.postMessage(JSON.stringify(fileSystem.files.children))
+  }
+  createWorker = (f) => {
+    var blob = new Blob(['(' + f.toString() + ')()']);
+    var url = window.URL.createObjectURL(blob);
+    var worker = new Worker(url);
+    return worker;
   }
 }
 const workerStore = new WorkerStore()
