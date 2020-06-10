12f7f3729dfc0a0085806d2087ac4991d7e8c5bb
yunliang <yunliang@ding.com>
2020-06-10 21:11:56 +0800

    样式冲突

--- src/component/footer/index.tsx
+++ src/component/footer/index.tsx
@@ -11,7 +11,7 @@ class Footer extends React.Component<any, any> {
   render() {
     const { git: { waitCommitCount, branch, isGitProject } } = this.props.Git
     let theme = this.props.UI.isDark ? '-dark' : ''
-    return <div className={`app-footer${theme}`}>
+    return <div className={`app-workbench-footer${theme}`}>
       {
         isGitProject && <div>
           {branch}    {waitCommitCount}    <i className='iconfont icon-jiantou'></i>
